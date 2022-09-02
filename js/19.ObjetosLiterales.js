477874949368f7c0191a13f2ee292d962fb54214
Luis Garcia <lg_20_GA@hotmail.com>
2022-09-01 23:42:43 -0500

    objetos literales
    objetos literales

--- js/19.ObjetosLiterales.js
+++ js/19.ObjetosLiterales.js
@@ -1,12 +1,25 @@
 let nombre = "tiago",
   edad = 7;
-console.log(nombre, edad);
 
 let perro = {
-  nombre,
-  edad,
-  ladrar() {
+  nombre: nombre,
+  edad: edad,
+  ladrar: function () {
     console.log("guau guau");
   },
 };
 console.log(perro);
+perro.ladrar();
+
+//---------simplificamos-------------------------
+
+let dog = {
+  nombre,
+  edad,
+  raza: "Callejero",
+  ladrar() {
+    console.log("guau guau GUAUUUU");
+  },
+};
+console.log(dog);
+dog.ladrar();
